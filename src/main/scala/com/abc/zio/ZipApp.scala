c93795c907cc9ff46f0eb3de0945410c4baa0fdd
Brian Xiang <brian.xiang@gmail.com>
2019-05-15 16:23:13 -0400

    Add files via upload

---
+++ src/main/scala/com/abc/zio/ZipApp.scala
@@ -0,0 +1,17 @@
+package com.abc.zio
+
+import scalaz.zio.App
+import scalaz.zio.console._
+
+object ZipApp extends App {
+
+  def run(args: List[String]) =
+    myAppLogic.fold(_ => 1, _ => 0)
+
+  val myAppLogic =
+    for {
+      _ <- putStrLn("Hello! What is your name?")
+      n <- getStrLn
+      _ <- putStrLn(s"Hello, ${n}, welcome to ZIO!")
+    } yield ()
+}
